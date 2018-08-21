41f136c2e1d317b92357729ec9c555167a1c8ea7
Mikita Halatsevich <Mikita_Halatsevich@epam.com>
2018-08-21 13:09:23 +0300

    Refactoring properties files

--- src/main/java/com/epam/esauto/spring/AppConfig.java
+++ src/main/java/com/epam/esauto/spring/AppConfig.java
@@ -1,14 +1,18 @@
 package com.epam.esauto.spring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan(basePackages =  "com.epam.esauto")
-@PropertySource("classpath:testconfig.properties")
+@PropertySources({
+        @PropertySource("classpath:mainpage.properties"),
+        @PropertySource("classpath:editprofile.properties"),
+        @PropertySource("classpath:content.properties"),
+        @PropertySource("classpath:homeprop.properties"),
+        @PropertySource("classpath:login.properties"),
+        @PropertySource("classpath:registration.properties"),
+        @PropertySource("classpath:yandex.properties")})
 public class AppConfig {
 
     @Bean
