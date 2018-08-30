a6d370a337047827d51c5d6ddb9921b99801d7d2
Mikita Halatsevich <Mikita_Halatsevich@epam.com>
2018-08-30 11:51:36 +0300

    Minor refactoring

--- src/main/java/com/epam/esauto/spring/AppConfig.java
+++ src/main/java/com/epam/esauto/spring/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = "com.epam.esauto")
-@PropertySource({"classpath:mainpage.properties", "classpath:editprofile.properties", "classpath:content.properties",
+@PropertySource({"classpath:editprofile.properties", "classpath:content.properties",
         "classpath:homeprop.properties", "classpath:login.properties", "classpath:registration.properties",
         "classpath:headerFooter.properties", "classpath:yandex.properties"})
 public class AppConfig {
